0d96171d241f477a7f9b683858fbff6d14fbf657
ramjeet51 <ramjeetprajapaty777@gmail.com>
2024-12-19 14:55:01 +0530

    AVP-4 : full call handing on server side

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_socketio import SocketIO, emit
 import uuid
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -10,7 +12,20 @@ def index():
 
 @app.route('/call/<link>')
 def call(link):
-    return render_template('video_call.html')  # Display the video call UI
+    return render_template('index.html')  # Display the video call UI
+
+@socketio.on('offer')
+def handle_offer(data):
+    emit('offer', data, broadcast=True)
+
+@socketio.on('answer')
+def handle_answer(data):
+    emit('answer', data, broadcast=True)
+
+@socketio.on('candidate')
+def handle_candidate(data):
+    emit('candidate', data, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
+
